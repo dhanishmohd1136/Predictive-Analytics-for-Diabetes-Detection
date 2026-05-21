3a3ac7da9cf09f96cbe9be7e31f3598b5dda1b58
dhanishmohd1136 <dhanishmohd1136@gmail.com>
2026-05-21 19:05:09 +0530

    Fixed missing joblib import

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,3 +1,14 @@
 import streamlit as st
 import pandas as pd
+import joblib
 
+model = joblib.load(
+    "models/best_tuned_model.pkl"
+)
+
+scaler = joblib.load(
+    "models/scaler.pkl"
+)
+
+model = joblib.load(...)
+scaler = joblib.load(...)
